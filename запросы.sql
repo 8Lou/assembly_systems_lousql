00e82b6eb9959dd3e7061dfa47608c2a69263e68
8Lou <lou8@mail.ru>
2024-02-20 23:39:41 +0300

    CREATE PROCEDURE

--- запросы.sql
+++ запросы.sql
@@ -28,3 +28,66 @@ VALUES
 (2, 1, 3, 2),
 (3, 2, 2, 3),
 (4, 2, 4, 1);
+DELIMITER //
+
+CREATE PROCEDURE Добавить_Заказ (
+    IN номер_заказа NVARCHAR(50),
+    IN дата_заказа DATE,
+    IN id_номенклатуры INT,
+    IN количество INT
+)
+BEGIN
+    DECLARE id_заказа INT;
+
+    INSERT INTO Заказы (номер_заказа, дата_заказа)
+    VALUES (номер_заказа, дата_заказа);
+
+    SET id_заказа = LAST_INSERT_ID();
+
+    INSERT INTO Состав_заказа (id_заказа, id_номенклатуры, количество)
+    VALUES (id_заказа, id_номенклатуры, количество);
+END //
+
+DELIMITER //
+
+CREATE PROCEDURE Добавить_Заказ (
+    IN номер_заказа CHAR(50) CHARACTER SET utf8mb4,
+    IN дата_заказа DATE,
+    IN id_номенклатуры INT,
+    IN количество INT
+)  
+BEGIN
+    DECLARE id_заказа INT;
+
+    INSERT INTO Заказы (номер_заказа, дата_заказа)  
+    VALUES (номер_заказа, дата_заказа); 
+
+    SET id_заказа = LAST_INSERT_ID(); 
+
+    INSERT INTO Состав_заказа (id_заказа, id_номенклатуры, количество)  
+    VALUES (id_заказа, id_номенклатуры, количество); 
+END //
+
+DELIMITER ;
+
+SELECT * FROM Заказы;
+CALL Добавить_Заказ('Заказ 3', '2022-08-01', 1, 3);
+CALL Добавить_Заказ('Заказ 4', '2022-08-05', 2, 5);
+DROP PROCEDURE IF EXISTS Добавить_Заказ;
+INSERT INTO Заказы (id, номер_заказа, дата_заказа)
+VALUES (5, 'Заказ 5', '2022-08-10');
+DELIMITER //
+CREATE FUNCTION CountItemsOnAssemblySites()
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE количество_номенклатуры INT;
+    
+    SELECT COUNT(o.id) INTO количество_номенклатуры
+    FROM Сборочные_площадки p
+    LEFT JOIN Заказы o ON p.id = o.id_площадки
+    GROUP BY p.id;
+
+    RETURN количество_номенклатуры;
+END //
+DELIMITER ;

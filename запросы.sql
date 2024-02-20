b7a22c226682b17cc532552cfd4e010ec44e3962
8Lou <lou8@mail.ru>
2024-02-21 00:01:07 +0300

    create functions

--- запросы.sql
+++ запросы.sql
@@ -91,3 +91,94 @@ BEGIN
     RETURN количество_номенклатуры;
 END //
 DELIMITER ;
+DELIMITER //
+CREATE FUNCTION CreateAssemblyPlan(площадка INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE план INT;
+    
+    SELECT COUNT(id_заказа) INTO план
+    FROM Состав_заказа
+    WHERE id_заказа = (SELECT id FROM Заказы WHERE id_площадки = площадка);
+    
+    RETURN план;
+END //
+
+DELIMITER ;
+
+создать еще одну функцию для определения возможности выполнения заказа. Эта функция будет проверять, достаточно ли свободных остатков и времени на сборке для выполнения заказа на каждой сборочной площадке.
+
+Вот пример того, как можно создать функцию для определения возможности выполнения заказа:
+
+Picture of the author
+DELIMITER //
+
+CREATE FUNCTION CheckOrderFeasibility(площадка INT)
+RETURNS BOOLEAN
+DETERMINISTIC
+BEGIN
+    DECLARE возможность BOOLEAN;
+    
+    IF EXISTS (SELECT * FROM Свободные_остатки WHERE id_площадки = площадка) AND целесообразность_сборки(площадка) = TRUE THEN
+        SET возможность = TRUE;
+    ELSE
+        SET возможность = FALSE;
+    END IF;
+    
+    RETURN возможность;
+END //
+
+DELIMITER ;
+Создайте функцию, которая будет определять общее время, необходимое для сборки всех номенклатур заказа на определенной сборочной площадке.
+
+Пример функции:
+Picture of the author
+DELIMITER //
+
+CREATE FUNCTION CalculateAssemblyTime(площадка INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE время_сборки INT;
+    
+    SELECT SUM(срок_сборки) INTO время_сборки
+    FROM Состав_заказа
+    INNER JOIN orders ON Состав_заказа.id_заказа = orders.id
+    WHERE id_площадки = площадка;
+    
+    RETURN время_сборки;
+END //
+
+DELIMITER ;
+DELIMITER //
+
+CREATE PROCEDURE GetPickUpPlaces(заказ INT)
+BEGIN
+    SELECT p.id AS id_площадки, SUM(s.количество) AS количество
+    FROM Состав_заказа s
+    JOIN Сборочные_площадки p ON s.id_площадки = p.id
+    WHERE s.id_заказа = заказ
+    GROUP BY p.id;
+END //
+
+DELIMITER ;
+DELIMITER //
+
+CREATE FUNCTION FinalAssemblyTimeEstimate(заказ INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE время_сборки INT;
+    
+    SELECT SUM(срок_сборки) INTO время_сборки
+    FROM Состав_заказа s
+    JOIN Свободные_остатки o ON s.id_номенклатуры = o.id_номенклатуры
+    WHERE s.id_заказа = заказ;
+    
+    RETURN время_сборки;
+END //
+
+DELIMITER ;
+SET @заказ := 45; 
+CALL GetPickUpPlaces(@заказ); 
